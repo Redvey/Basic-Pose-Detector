25fcc2227d71deb839400011b0bfe56b5e3bff37
redvey <roopam12cse@gmail.com>
2025-07-08 10:47:07 +0530

    05:feat(core): add responsive layout to login screen fix(auth): resolve form validation error chore: update dependencies

---
+++ lib/core/themes/theme.dart
@@ -0,0 +1,42 @@
+// import 'package:flutter/material.dart';
+//
+// import '../constants/color_strings.dart';
+//
+//
+// class AppTheme {
+//   AppTheme._();
+//
+//   static ThemeData lightTheme = ThemeData(
+//     useMaterial3: true,
+//     fontFamily: 'Poppins',
+//     disabledColor: AppColors.grey,
+//     brightness: Brightness.light,
+//     primaryColor: AppColors.primary,
+//     textTheme: CarterTextTheme.lightTextTheme,
+//     chipTheme: CarterChipTheme.lightChipTheme,
+//     scaffoldBackgroundColor: AppColors.white,
+//     appBarTheme: CarterAppBarTheme.lightAppBarTheme,
+//     checkboxTheme: CarterCheckboxTheme.lightCheckboxTheme,
+//     bottomSheetTheme: CarterBottomSheetTheme.lightBottomSheetTheme,
+//     elevatedButtonTheme: CarterElevatedButtonTheme.lightElevatedButtonTheme,
+//     outlinedButtonTheme: CarterOutlinedButtonTheme.lightOutlinedButtonTheme,
+//     inputDecorationTheme: CarterTextFormFieldTheme.lightInputDecorationTheme,
+//   );
+//
+//   static ThemeData darkTheme = ThemeData(
+//     useMaterial3: true,
+//     fontFamily: 'Poppins',
+//     disabledColor: AppColors.grey,
+//     brightness: Brightness.dark,
+//     primaryColor: AppColors.primary,
+//     textTheme: CarterTextTheme.darkTextTheme,
+//     chipTheme: CarterChipTheme.darkChipTheme,
+//     scaffoldBackgroundColor: AppColors.black,
+//     appBarTheme: CarterAppBarTheme.darkAppBarTheme,
+//     checkboxTheme: CarterCheckboxTheme.darkCheckboxTheme,
+//     bottomSheetTheme: CarterBottomSheetTheme.darkBottomSheetTheme,
+//     elevatedButtonTheme: CarterElevatedButtonTheme.darkElevatedButtonTheme,
+//     outlinedButtonTheme: CarterOutlinedButtonTheme.darkOutlinedButtonTheme,
+//     inputDecorationTheme: CarterTextFormFieldTheme.darkInputDecorationTheme,
+//   );
+// }
---
+++ lib/core/themes/widget_themes/app_bar_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/app_sizes.dart';
+import '../../constants/color_strings.dart';
+
+
+class ThisAppBarTheme{
+  ThisAppBarTheme._();
+
+  static const lightAppBarTheme = AppBarTheme(
+    elevation: 0,
+    centerTitle: false,
+    scrolledUnderElevation: 0,
+    backgroundColor: Colors.transparent,
+    surfaceTintColor: Colors.transparent,
+    iconTheme: IconThemeData(color: AppColors.black, size: AppSizes.iconMd),
+    actionsIconTheme: IconThemeData(color: AppColors.black, size: AppSizes.iconMd),
+    titleTextStyle: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600, color: AppColors.black),
+  );
+  static const darkAppBarTheme = AppBarTheme(
+    elevation: 0,
+    centerTitle: false,
+    scrolledUnderElevation: 0,
+    backgroundColor: Colors.transparent,
+    surfaceTintColor: Colors.transparent,
+    iconTheme: IconThemeData(color: AppColors.black, size: AppSizes.iconMd),
+    actionsIconTheme: IconThemeData(color: AppColors.white, size: AppSizes.iconMd),
+    titleTextStyle: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600, color: AppColors.white),
+  );
+}
